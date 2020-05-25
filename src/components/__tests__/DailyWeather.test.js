b21f0f34ed3cfce22eb739391b7ed907920d563d
amud <amudhabalan@gmail.com>
2020-05-25 16:51:11 +1000

    Updates Jest Tests

--- src/components/__tests__/DailyWeather.test.js
+++ src/components/__tests__/DailyWeather.test.js
@@ -1,51 +1,74 @@
-import React from "react";
-import DailyWeather from "../DailyWeather";
-import Adapter from "enzyme-adapter-react-16";
-import { configure, shallow } from "enzyme";
-import { Typography } from "@material-ui/core";
+import React from 'react';
+import DailyWeather from '../DailyWeather';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from 'enzyme';
 
 configure({ adapter: new Adapter() });
 
 const weather = {
-  id: 5814427125284864,
-  weather_state_name: "Light Cloud",
-  weather_state_abbr: "lc",
-  wind_direction_compass: "SSE",
-  created: "2020-02-05T09:41:55.018446Z",
-  applicable_date: "2020-02-05",
-  min_temp: 14.245,
-  max_temp: 21.66,
-  the_temp: 20.700000000000003,
-  wind_speed: 6.767499617808002,
-  wind_direction: 156.17734157789738,
-  air_pressure: 1019.5,
-  humidity: 69,
-  visibility: 13.588455917442138,
-  predictability: 70
+  dt: 1590354000,
+  sunrise: 1590330189,
+  sunset: 1590382241,
+  temp: {
+    day: 16.98,
+    min: 13.88,
+    max: 16.98,
+    night: 16.88,
+    eve: 16.98,
+    morn: 16.98
+  },
+  feels_like: {
+    day: 14.62,
+    night: 16.09,
+    eve: 14.62,
+    morn: 14.62
+  },
+  pressure: 1021,
+  humidity: 93,
+  dew_point: 15.84,
+  wind_speed: 6.12,
+  wind_deg: 221,
+  weather: [
+    {
+      id: 804,
+      main: 'Clouds',
+      description: 'overcast clouds',
+      icon: '04n'
+    }
+  ],
+  clouds: 100,
+  uvi: 9.92
 };
 
-describe("DailyWeather", () => {
+describe('DailyWeather', () => {
   const wrapper = shallow(<DailyWeather weather={weather} />);
-  it("should Match Snapshot", () => {
+  it('should Match Snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it("should render the correct daily max and min temperature", () => {
+  it('should render the correct daily max and min temperature', () => {
     expect(
       wrapper.contains(
-        <Typography variant="overline">
-          <span>22&#8451;</span> <span>14&#8451;</span>
-        </Typography>
+        <React.Fragment>
+          <p>
+            17
+            <span>&#8451;</span> max
+          </p>
+          <p>
+            14
+            <span>&#8451;</span> min
+          </p>
+        </React.Fragment>
       )
     ).toEqual(true);
   });
 
-  it("should render the correct weather image", () => {
+  it('should render the correct weather image', () => {
     expect(
       wrapper.contains(
         <img
-          src={"https://www.metaweather.com/static/img/weather/ico/lc.ico"}
-          alt="Light Cloud"
+          src={'http://openweathermap.org/img/wn/04n@2x.png'}
+          alt="overcast clouds"
         ></img>
       )
     ).toEqual(true);
